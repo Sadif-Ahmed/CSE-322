aaac611d65acdd2a5a3736bc61a97e1d6c4f3bd2
Sadif-Ahmed <1905058@ugrad.cse.buet.ac.bd>
2023-08-11 21:03:47 +0600

    forking update

--- Offline 2/1905058_1.cc
+++ Offline 2/1905058_1.cc
@@ -38,7 +38,7 @@
 
 using namespace ns3;
 
-NS_LOG_COMPONENT_DEFINE("Mobile High Speed Wifi");
+NS_LOG_COMPONENT_DEFINE("Static High Speed Wifi");
 
 ApplicationContainer sender_apps;
 ApplicationContainer receiver_apps;
@@ -71,7 +71,7 @@ void delivery_ratio_calculation()
 void througput_calculation()
 {
     double time = Simulator::Now().ToDouble(Time::S);
-    throughput = (num_received_bits-recent_num_received_bits)/(time-recent_time)/1e6 ;
+    throughput = (num_received_bits-recent_num_received_bits)/(time-recent_time)/1e6;
     std::string msg ="At time " +std::to_string(time)+ " Throughput : " + std::to_string(throughput);
     NS_LOG_UNCOND(msg<<" Mbit/s");
     recent_num_received_bits=num_received_bits;
@@ -79,22 +79,27 @@ void througput_calculation()
     Simulator::Schedule(MilliSeconds(500), &througput_calculation);
 }
 
-class mobile_wifi
+
+class static_wifi
 {
     public:
     uint32_t nWifi;
     uint32_t flow;
-    uint32_t packets_per_second; 
+    uint32_t packets_per_second;
+    uint32_t coverage_area;
+    uint32_t tx_range; 
     uint32_t packet_size; 
-    uint32_t speed;
+    double dimension;
           
-    mobile_wifi(uint32_t nWifi,uint32_t flow,uint32_t packets_per_second,uint32_t packet_size,uint32_t speed)
+    static_wifi(uint32_t nWifi,uint32_t flow,uint32_t packets_per_second,uint32_t coverage_area,uint32_t tx_range,uint32_t packet_size)
     {
         this->nWifi=nWifi;
         this->flow=flow;
         this->packets_per_second=packets_per_second;
+        this->coverage_area=coverage_area;
+        this->tx_range=tx_range;
         this->packet_size=packet_size;
-        this->speed=speed;
+        this->dimension=coverage_area*tx_range*0.25;
     }
     void setup_topology_generate_flow()
     {
@@ -112,22 +117,22 @@ class mobile_wifi
     p2pDevices = pointToPoint.Install(p2pNodes);
 
     NodeContainer sender_wifiStaNodes;
-    sender_wifiStaNodes.Create((nWifi-2)/2);
+    sender_wifiStaNodes.Create((nWifi)/2);
     NodeContainer sender_wifiApNode = p2pNodes.Get(0);
 
     NodeContainer receiver_wifiStaNodes;
-    receiver_wifiStaNodes.Create((nWifi-2)/2);
+    receiver_wifiStaNodes.Create((nWifi)/2);
     NodeContainer receiver_wifiApNode = p2pNodes.Get(1);
 
     // Physical Layer
     // YANS model - Yet Another Network Simulator
     YansWifiChannelHelper sender_channel = YansWifiChannelHelper::Default();
-    sender_channel.AddPropagationLoss("ns3::RangePropagationLossModel");
+    sender_channel.AddPropagationLoss("ns3::RangePropagationLossModel", "MaxRange", ns3::DoubleValue(coverage_area * tx_range));
     YansWifiPhyHelper sender_phy;
     sender_phy.SetChannel(sender_channel.Create()); // share the same wireless medium 
 
     YansWifiChannelHelper receiver_channel = YansWifiChannelHelper::Default();
-    receiver_channel.AddPropagationLoss("ns3::RangePropagationLossModel");
+    receiver_channel.AddPropagationLoss("ns3::RangePropagationLossModel", "MaxRange", ns3::DoubleValue(coverage_area * tx_range));
     YansWifiPhyHelper receiver_phy;
     receiver_phy.SetChannel(receiver_channel.Create());
 
@@ -166,37 +171,23 @@ class mobile_wifi
 
     MobilityHelper mobility;
 
-    mobility.SetPositionAllocator ("ns3::RandomRectanglePositionAllocator",
-    "X", StringValue("ns3::UniformRandomVariable[Min=0.0|Max="+std::to_string((nWifi)/10)+"]"),
-    "Y", StringValue("ns3::UniformRandomVariable[Min=0.0|Max="+std::to_string((nWifi)/10)+"]"));
-
-    mobility.SetMobilityModel(
-            "ns3::RandomWalk2dMobilityModel",
-            "Mode",StringValue("Time"),
-            "Time",StringValue("1s"),
-            "Speed",StringValue("ns3::ConstantRandomVariable[Constant=" + std::to_string(speed)+"]"),
-            "Bounds",RectangleValue(Rectangle(0, ((nWifi)/2), 0, ((nWifi)/2))));
-    
-    mobility.Install(sender_wifiStaNodes);
-    mobility.SetMobilityModel ("ns3::RandomDirection2dMobilityModel",
-                              "Bounds", RectangleValue(Rectangle(0, ((nWifi)/2), 0, ((nWifi)/2))),
-                              "Speed", StringValue("ns3::ConstantRandomVariable[Constant=" + std::to_string(speed)+"]"),
-                              "Pause", StringValue ("ns3::ConstantRandomVariable[Constant=0.2]"));
-    mobility.Install(receiver_wifiStaNodes);
-
-    //  To get a mobility model and print their position
-    //  Ptr<MobilityModel> mob = sender_wifiStaNodes.Get(1)->GetObject<MobilityModel>();
-    //  std::cout << mob->GetPosition().x << " " << mob->GetPosition().y << std::endl;
-
     mobility.SetMobilityModel("ns3::ConstantPositionMobilityModel");
 
+    ns3::Ptr<ns3::ListPositionAllocator> center = ns3::CreateObject<ns3::ListPositionAllocator>();
+
+    center->Add(ns3::Vector(0.0, 0.0, 0.0));
+    mobility.SetPositionAllocator(center);
     mobility.Install(sender_wifiApNode);
     mobility.Install(receiver_wifiApNode);
-   
+    mobility.SetPositionAllocator ("ns3::RandomRectanglePositionAllocator",
+    "X", StringValue("ns3::UniformRandomVariable[Min=" + std::to_string(dimension*(-1)) + "|Max=" + std::to_string(dimension) + "]"),
+    "Y", StringValue("ns3::UniformRandomVariable[Min=" + std::to_string(dimension*(-1)) + "|Max=" + std::to_string(dimension) + "]"));
+    mobility.Install(sender_wifiStaNodes);
+    mobility.Install(receiver_wifiStaNodes);
 
-    
-
-    
+    // To get a mobility model and print their position
+    // Ptr<MobilityModel> mob = wifiStaNodes.Get(1)->GetObject<MobilityModel>();
+    // std::cout << mob->GetPosition().x << " " << mob->GetPosition().y << std::endl;
 
     
     
@@ -226,23 +217,23 @@ class mobile_wifi
     /* Populate routing table */
     Ipv4GlobalRoutingHelper::PopulateRoutingTables();
     uint32_t i;
-    for(i=1;i<=flow;i++)
+    for(i=0;i<flow;i++)
     {
         OnOffHelper sender_helper("ns3::TcpSocketFactory", 
             (InetSocketAddress(
-                receiver_staInterfaces.GetAddress((i)%((nWifi-2)/2)), 9)));
+                receiver_staInterfaces.GetAddress((i)%((nWifi)/2)), 9)));
     sender_helper.SetAttribute("PacketSize", 
         UintegerValue(packet_size));
     sender_helper.SetAttribute("OnTime", StringValue(
         "ns3::ConstantRandomVariable[Constant=1]"));
     sender_helper.SetAttribute("OffTime", StringValue(
         "ns3::ConstantRandomVariable[Constant=0]"));
-    sender_helper.SetAttribute("DataRate", DataRateValue(DataRate(packets_per_second*packet_size)));
+    sender_helper.SetAttribute("DataRate", DataRateValue(DataRate(packets_per_second*packet_size*8)));
 
-    sender_apps.Add(sender_helper.Install(sender_wifiStaNodes.Get((i)%((nWifi-2)/2))));
+    sender_apps.Add(sender_helper.Install(sender_wifiStaNodes.Get((i)%((nWifi)/2))));
     }
     
-    for(i=0;i<(nWifi-2)/2;i++)
+    for(i=0;i<(nWifi)/2;i++)
     {
         PacketSinkHelper reciever_helper("ns3::TcpSocketFactory", ns3::InetSocketAddress(ns3::Ipv4Address::GetAny(), 9));
         receiver_apps.Add(reciever_helper.Install(receiver_wifiStaNodes.Get(i)));
@@ -257,35 +248,34 @@ class mobile_wifi
 int
 main(int argc, char* argv[])
 {
-    
-
     uint32_t nWifi = 100;
     uint32_t flow = 50;
     uint32_t packets_per_second = 100;
-    uint32_t speed = 5;
+    uint32_t coverage_area = 1;
+    uint32_t tx_range = 5; 
     uint32_t packet_size = 1024;           /* Transport layer payload size in bytes. */
     double simulationTime = 10;            /* Simulation time in seconds. */
-    uint32_t param=1;
-    
+    uint32_t param=0;
+
     CommandLine cmd(__FILE__);
     cmd.AddValue("nWifi", "Number of wifi STA devices", nWifi);
     cmd.AddValue("flow", "Number of flows", flow);
     cmd.AddValue("packets_per_second", "Number of packets per second", packets_per_second);
-    cmd.AddValue("speed", "Node Speeds", speed);
+    cmd.AddValue("coverage_area", "Coverage area", coverage_area);
     cmd.AddValue("param", "Parametre", param);
     cmd.Parse(argc, argv);
-
+    
     //LogComponentEnable("OnOffApplication", ns3::LOG_LEVEL_INFO);
     //LogComponentEnable("PacketSink", ns3::LOG_LEVEL_INFO);
     NS_LOG_UNCOND("Simulation Parametres:");
     NS_LOG_UNCOND("Number of wifi devices: "<<nWifi);
     NS_LOG_UNCOND("Number of flows: " << flow);
     NS_LOG_UNCOND("Number of packets per second: "<<packets_per_second);
-    NS_LOG_UNCOND("Node Speed: "<<speed);
+    NS_LOG_UNCOND("Coverage area: "<<coverage_area);
 
-
-    mobile_wifi *topology = new mobile_wifi(nWifi,flow,packets_per_second,packet_size,speed);
+    static_wifi *topology = new static_wifi(nWifi,flow,packets_per_second,coverage_area,tx_range,packet_size);
     topology->setup_topology_generate_flow();
+    
 
     uint32_t i;
 
@@ -306,9 +296,9 @@ main(int argc, char* argv[])
     Simulator::Schedule(Seconds(1.5), &througput_calculation);
     Simulator::Stop(Seconds(simulationTime+1));
     ns3::Simulator::Run();
-    //NS_LOG_UNCOND(num_sent_packet);
-    //NS_LOG_UNCOND(num_received_packet);
-    //plot("test");
+    // NS_LOG_UNCOND(num_sent_packet);
+    // NS_LOG_UNCOND(num_received_packet);
+    
     double final_time = Simulator::Now().ToDouble(Time::S);
     throughput = (num_received_bits)/final_time /1e6 ;
     std::string msg ="Average Throughput : " + std::to_string(throughput);
@@ -316,7 +306,6 @@ main(int argc, char* argv[])
     packet_delivery_ratio = (num_received_packet)/(num_sent_packet*1.0);
     msg = "Average Packet Delivery Ratio : "+ std::to_string(packet_delivery_ratio); 
     NS_LOG_UNCOND(msg);
-
     if(param == 1)
     {
         std::cout << nWifi << "\t" << throughput  << std::endl;
@@ -343,12 +332,13 @@ main(int argc, char* argv[])
     }
     else if(param == 7)
     {
-        std::cout << speed << "\t" << throughput  << std::endl;
+        std::cout << coverage_area*tx_range << "\t" << throughput  << std::endl;
     }
     else if(param == 8)
     {
-        std::cout << speed << "\t" << packet_delivery_ratio  << std::endl;
+        std::cout << coverage_area*tx_range << "\t" << packet_delivery_ratio  << std::endl;
     }
+    
 
     ns3::Simulator::Destroy();
     return 0;
--- Offline 2/1905058_2.sh
+++ Offline 2/1905058_2.sh
@@ -3,12 +3,13 @@ temp_n=20
 temp_f=10
 temp_p=100
 temp_s=5
+rm -rf "scratch/mobilegraphs" >> /dev/null
 mkdir "scratch/mobilegraphs"
 #Varying Number of Nodes
 for i in {1..5}
 do
-    ./ns3 run --quiet "scratch/mobilewifi.cc --nWifi=$temp_n --flow=$temp_f --packets_per_second=100 --speed=5 --param=1" >> "scratch/mobilegraphs/node_throughput.dat"
-    ./ns3 run --quiet "scratch/mobilewifi.cc --nWifi=$temp_n --flow=$temp_f --packets_per_second=100 --speed=5 --param=2" >> "scratch/mobilegraphs/node_delivery.dat"
+    ./ns3 run --quiet "scratch/1905058_2.cc --nWifi=$temp_n --flow=$temp_f --packets_per_second=100 --speed=5 --param=1" >> "scratch/mobilegraphs/node_throughput.dat"
+    ./ns3 run --quiet "scratch/1905058_2.cc --nWifi=$temp_n --flow=$temp_f --packets_per_second=100 --speed=5 --param=2" >> "scratch/mobilegraphs/node_delivery.dat"
     temp_n=$(( $temp_n + 20 ))
     temp_f=$(( $temp_f + 10 ))
 done
@@ -17,7 +18,6 @@ set terminal png
 set output "scratch/mobilegraphs/nodes_vs_throughput.png"
 set xlabel "Number of Nodes"
 set ylabel "Throughput(Mbit/s)"
-set yrange [0:10]
 plot "scratch/mobilegraphs/node_throughput.dat" using 1:2 title 'NodeCount VS Throughput' with linespoints
 EOFMarker
 gnuplot -persist <<-EOFMarker
@@ -25,15 +25,14 @@ set terminal png
 set output "scratch/mobilegraphs/nodes_vs_deliveryratio.png"
 set xlabel "Number of Nodes"
 set ylabel "Packet Delivery Ratio"
-set yrange [0.50:1.25]
 plot "scratch/mobilegraphs/node_delivery.dat" using 1:2 title 'NodeCount VS DeliveryRatio' with linespoints
 EOFMarker
 #Varying Number of Flows
 temp_f=10
 for i in {1..5}
 do
-    ./ns3 run --quiet "scratch/mobilewifi.cc --nWifi=20 --flow=$temp_f --packets_per_second=100 --speed=5 --param=3" >> "scratch/mobilegraphs/flow_throughput.dat"
-    ./ns3 run --quiet "scratch/mobilewifi.cc --nWifi=20 --flow=$temp_f --packets_per_second=100 --speed=5 --param=4" >> "scratch/mobilegraphs/flow_delivery.dat"
+    ./ns3 run --quiet "scratch/1905058_2.cc --nWifi=20 --flow=$temp_f --packets_per_second=100 --speed=5 --param=3" >> "scratch/mobilegraphs/flow_throughput.dat"
+    ./ns3 run --quiet "scratch/1905058_2.cc --nWifi=20 --flow=$temp_f --packets_per_second=100 --speed=5 --param=4" >> "scratch/mobilegraphs/flow_delivery.dat"
     temp_f=$(( $temp_f + 10 ))
 done
 gnuplot -persist <<-EOFMarker
@@ -41,7 +40,6 @@ set terminal png
 set output "scratch/mobilegraphs/flow_vs_throughput.png"
 set xlabel "Number of Flows"
 set ylabel "Throughput(Mbit/s)"
-set yrange [0:10]
 plot "scratch/mobilegraphs/flow_throughput.dat" using 1:2 title 'FlowCount VS Throughput' with linespoints
 EOFMarker
 gnuplot -persist <<-EOFMarker
@@ -49,15 +47,14 @@ set terminal png
 set output "scratch/mobilegraphs/flow_vs_deliveryratio.png"
 set xlabel "Number of Flows"
 set ylabel "Packet Delivery Ratio"
-set yrange [0.50:1.25]
 plot "scratch/mobilegraphs/flow_delivery.dat" using 1:2 title 'FlowCount VS DeliveryRatio' with linespoints
 EOFMarker
 #Varying Number of Packets Per Second
 temp_p=100
 for i in {1..5}
 do
-    ./ns3 run --quiet "scratch/mobilewifi.cc --nWifi=20 --flow=10 --packets_per_second=$temp_p --speed=5 --param=5" >> "scratch/mobilegraphs/packet_throughput.dat"
-    ./ns3 run --quiet "scratch/mobilewifi.cc --nWifi=20 --flow=10 --packets_per_second=$temp_p --speed=5 --param=6" >> "scratch/mobilegraphs/packet_delivery.dat"
+    ./ns3 run --quiet "scratch/1905058_2.cc --nWifi=20 --flow=10 --packets_per_second=$temp_p --speed=5 --param=5" >> "scratch/mobilegraphs/packet_throughput.dat"
+    ./ns3 run --quiet "scratch/1905058_2.cc --nWifi=20 --flow=10 --packets_per_second=$temp_p --speed=5 --param=6" >> "scratch/mobilegraphs/packet_delivery.dat"
     temp_p=$(( $temp_p + 100 ))
 done
 gnuplot -persist <<-EOFMarker
@@ -65,7 +62,6 @@ set terminal png
 set output "scratch/mobilegraphs/packets_vs_throughput.png"
 set xlabel "Number of Packets per Second"
 set ylabel "Throughput(Mbit/s)"
-set yrange [0:10]
 plot "scratch/mobilegraphs/packet_throughput.dat" using 1:2 title 'PacketperSecondCount VS Throughput' with linespoints
 EOFMarker
 gnuplot -persist <<-EOFMarker
@@ -73,15 +69,14 @@ set terminal png
 set output "scratch/mobilegraphs/packet_vs_deliveryratio.png"
 set xlabel "Number of Packets per Second"
 set ylabel "Packet Delivery Ratio"
-set yrange [0.50:1.25]
 plot "scratch/mobilegraphs/packet_delivery.dat" using 1:2 title 'PacketperSecondCount VS DeliveryRatio' with linespoints
 EOFMarker
 #Varying speed Area
 temp_s=5
 for i in {1..5}
 do
-    ./ns3 run --quiet "scratch/mobilewifi.cc --nWifi=20 --flow=10 --packets_per_second=100 --speed=$temp_s --param=7" >> "scratch/mobilegraphs/speed_throughput.dat"
-    ./ns3 run --quiet "scratch/mobilewifi.cc --nWifi=20 --flow=10 --packets_per_second=100 --speed=$temp_s --param=8" >> "scratch/mobilegraphs/speed_delivery.dat"
+    ./ns3 run --quiet "scratch/1905058_2.cc --nWifi=20 --flow=10 --packets_per_second=100 --speed=$temp_s --param=7" >> "scratch/mobilegraphs/speed_throughput.dat"
+    ./ns3 run --quiet "scratch/1905058_2.cc --nWifi=20 --flow=10 --packets_per_second=100 --speed=$temp_s --param=8" >> "scratch/mobilegraphs/speed_delivery.dat"
     temp_s=$(( $temp_s + 5 ))
 done
 gnuplot -persist <<-EOFMarker
@@ -89,7 +84,6 @@ set terminal png
 set output "scratch/mobilegraphs/speed_vs_throughput.png"
 set xlabel "Speed"
 set ylabel "Throughput(Mbit/s)"
-set yrange [0:10]
 plot "scratch/mobilegraphs/speed_throughput.dat" using 1:2 title 'Speed VS Throughput' with linespoints
 EOFMarker
 gnuplot -persist <<-EOFMarker
@@ -97,6 +91,5 @@ set terminal png
 set output "scratch/mobilegraphs/speed_vs_deliveryratio.png"
 set xlabel "Speed"
 set ylabel "Packet Delivery Ratio"
-set yrange [0.50:1.25]
 plot "scratch/mobilegraphs/speed_delivery.dat" using 1:2 title 'Speed VS DeliveryRatio' with linespoints
 EOFMarker
